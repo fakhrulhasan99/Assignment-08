8006890d42575c242ee545b56182e45131559f5f
fakhrulhasan99 <fakhrulhasan.faf1@gmail.com>
2026-02-14 11:20:44 +0600

    added navbar

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
-        <div>
+        <div className='md:max-w-[95%] lg:max-w-[90%] mx-auto'>
             <Navbar />
             <Outlet />
         </div>
